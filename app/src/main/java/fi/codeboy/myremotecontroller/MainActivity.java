2abdbc99373375f3602a9f49634b95ccf8c2fb48
Tuomas Jaakola <tuomas.jaakola@iki.fi>
2024-01-03 09:44:55 +0200

    Vol Up works

--- app/src/main/java/fi/codeboy/myremotecontroller/MainActivity.java
+++ app/src/main/java/fi/codeboy/myremotecontroller/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.hardware.ConsumerIrManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     int FREQUENCY = 38000;
@@ -18,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         irManager = (ConsumerIrManager) getSystemService(CONSUMER_IR_SERVICE);
+
+        Button volUp = findViewById(R.id.btn_vol_up);
+        volUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendIRSignal(FREQUENCY, IR_VOL_UP);
+            }
+        });
     }
 
     private void sendIRSignal(int frequency, int[] pattern) {
