38747ad3a5016738f8e91cce9aae7eb36d2a5fe1
Tuomas Jaakola <tuomas.jaakola@iki.fi>
2024-01-03 09:38:48 +0200

    MainActivity.sendIRSignal

--- app/src/main/java/fi/codeboy/myremotecontroller/MainActivity.java
+++ app/src/main/java/fi/codeboy/myremotecontroller/MainActivity.java
@@ -2,13 +2,29 @@ package fi.codeboy.myremotecontroller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.ConsumerIrManager;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    int FREQUENCY = 38000;
+    int[] IR_VOL_UP = {4512, 4512, 564, 564, 564, 1692, 564, 1692, 564, 564, 564, 564, 564, 564, 564, 564, 564, 1692, 564, 564, 564, 1692, 564, 1692, 564, 564, 564, 1692, 564, 1692, 564, 1692, 564, 564, 564, 564, 564, 564, 564, 1692, 564, 564, 564, 1692, 564, 564, 564, 1692, 564, 564, 564, 1692, 564, 1692, 564, 564, 564, 1692, 564, 564, 564, 1692, 564, 564, 564, 1692, 564, 43992, 4512, 4512, 564, 1692, 564, 95880};
+
+    private ConsumerIrManager irManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        irManager = (ConsumerIrManager) getSystemService(CONSUMER_IR_SERVICE);
+    }
+
+    private void sendIRSignal(int frequency, int[] pattern) {
+        if (!irManager.hasIrEmitter()) {
+            Log.e("IR", "No IR emitter found on this device");
+            return;
+        }
+        irManager.transmit(frequency, pattern);
     }
 }
